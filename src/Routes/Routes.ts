f5793061ffd69127b75df771dff3cd5e322a219a
Anderson-Reges <andersonreges76@gmail.com>
2023-03-22 20:21:27 -0300

    fix: lint error

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -5,7 +5,7 @@ const routes = Router();
 
 routes.post(
   '/cars',
-  (req, res, next) => new CarController(req, res, next).create()
-)
+  (req, res, next) => new CarController(req, res, next).create(),
+);
 
-export default routes
+export default routes;
